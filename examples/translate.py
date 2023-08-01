41a6cef451c587ac19619c10190d1c98c42e965c
johnnysands <johnnysands@users.noreply.github.com>
2023-08-01 16:19:48 +0300

    add explanation

--- examples/translate.py
+++ examples/translate.py
@@ -1,3 +1,10 @@
+"""
+This example is just demonstrating the functionality of the execute_parallel method.
+
+Since the graph is a DAG, we can often execute nodes in parallel that do not depend
+on each other.  This is a simple example of how to do that that simply parallelizes
+the execution of a bunch of similar translation calls.
+"""
 import agentgraph
 import functools
 import openai
@@ -20,24 +27,26 @@ def translate(language, input):
     return completion("Translate to " + language + ": " + input)
 
 
+# create a node for each language
+languages = [
+    "Spanish",
+    "Russian",
+    "French",
+    "Italian",
+    "German",
+    "Swedish",
+    "Norwegian",
+    "Danish",
+    "Finnish",
+    "Portugese",
+]
+
+
 def main():
     # create graph and top level input node
     dag = agentgraph.DAG()
     dag.add_node(agentgraph.InputNode("input"))
 
-    # create a node for each language
-    languages = [
-        "Spanish",
-        "Russian",
-        "French",
-        "Italian",
-        "German",
-        "Swedish",
-        "Norwegian",
-        "Danish",
-        "Finnish",
-        "Portugese",
-    ]
     for language in languages:
         fn = functools.partial(translate, language)
         dag.add_node(agentgraph.Node(language, fn))
